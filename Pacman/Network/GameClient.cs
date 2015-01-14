66c3f752f1fb9d1103a7909c54add969fc12af73
Maurice Manshanden <m.manshanden@gmail.com>
2015-01-14 13:20:25 +0100

    Removed all traces of netplayer

--- Pacman/Network/GameClient.cs
+++ Pacman/Network/GameClient.cs
@@ -18,12 +18,6 @@ namespace Network
         private NetClient client;
         private NetIncomingMessage inc;
 
-        private List<NetPlayer> sendData;
-        private List<NetPlayer> receivedData;
-
-        public bool IsHost { get; private set; }
-        public int ClientNetworkId { get; set; }
-
         public GameClient()
         {
             this.loginReplyReceived = false;
@@ -47,28 +41,13 @@ namespace Network
             this.client.Disconnect("leaving");
         }
 
-        public void SetData(NetPlayer netplayer)
-        {
-            if (this.sendData == null)
-                this.sendData = new List<NetPlayer>();
-
-            netplayer.ID = this.ClientNetworkId;
-            netplayer.Time = this.clientUpdateCount;
-            this.sendData.Add(netplayer);
-        }
-        public List<NetPlayer> GetData()
-        {
-            return this.receivedData;
-        }
-
         #region NetRoutine
         private void RecieveLogin()
         {
-            this.ClientNetworkId = inc.ReadInt32();
-            this.IsHost = inc.ReadBoolean();
+            this.clientNetworkId = inc.ReadInt32();
         }
 
-        private void ReceivePacket(PacketType type)
+        private void ReceiveMessage(PacketType type)
         {
             switch (type)
             {
@@ -98,7 +77,13 @@ namespace Network
 
         public void Update(float dt)
         {
-            this.timer = this.timer - dt;
+            this.timer -= dt;
+            if (this.timer < 0)
+            {
+                this.SendWorldState();
+                this.timer = UpdateTimer;
+            }
+            
             this.inc = this.client.ReadMessage();
 
             if (inc == null)
@@ -119,14 +104,9 @@ namespace Network
             if (!this.loginReplyReceived)
                 return;
 
-            this.ReceivePacket(type);
+            this.ReceiveMessage(type);
 
-            if (this.timer < 0)
-            {
-                this.SendWorldState();
-                this.timer = UpdateTimer;
-            }
-
+            
             this.sendData = null;
         }
         #endregion
