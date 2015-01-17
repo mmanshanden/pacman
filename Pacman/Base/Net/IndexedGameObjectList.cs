af3042d9d9fe1cf9a9e2e3233b88e0e2e1097bf2
Maurice Manshanden <m.manshanden@gmail.com>
2015-01-17 01:20:50 +0100

    Added network object list concepts

---
+++ Pacman/Base/Net/IndexedGameObjectList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Network;
+
+namespace Base
+{
+    class IndexedGameObjectList : GameObject
+    {
+        Dictionary<int, GameObject> gameObjects;
+
+        public IndexedGameObjectList()
+        {
+
+        }
+
+        public void Add(int id, GameObject gameObject)
+        {
+            this.gameObjects[id] = gameObject;
+        }
+
+        public bool Contains(int id)
+        {
+            return this.gameObjects.ContainsKey(id);
+        }
+
+        public void OverwriteGameObject(int id, NetMessage message)
+        {
+            // do stuff
+        }
+        public NetMessage CopyToMessage(int id)
+        {
+            // do stuff
+
+            return new NetMessage();
+        }
+    }
+}
---
+++ Pacman/Base/Net/OderedGameObjectList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Network;
+
+namespace Base.Net
+{
+    /// <summary>
+    /// Stores gameobjects and updates their netdata in 
+    /// the same order they were added.
+    /// </summary>
+    class OderedGameObjectList
+    {
+        
+        private int index;
+        private List<GameObject> gameObjects;
+
+        public OderedGameObjectList()
+        {
+            this.index = 0;
+            this.gameObjects = new List<GameObject>();
+        }
+
+        public void Add(GameObject gameObject)
+        {
+            this.gameObjects.Add(gameObject);
+        }
+
+        public void OverwriteGameObject(NetMessage netMessage)
+        {
+            // do stuff
+
+            this.index++;
+        }
+
+        public NetMessage CopyToMessage()
+        {
+            // do stuff
+
+            this.index++;
+            return new NetMessage();
+        }
+
+        public void Restart()
+        {
+            this.index = 0;
+        }
+
+        public override void Update(float dt)
+        {
+            this.index = 0;
+        }
+    }
+}
