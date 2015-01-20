1bc54dbe48af51833739cc0dd37b4d961c62b414
Maurice Manshanden <m.manshanden@gmail.com>
2015-01-20 22:01:28 +0100

    Added lobby join state

---
+++ Pacman/Pacman/Pacman/States/Multiplayer/Client/StateJoinLobby.cs
@@ -0,0 +1,36 @@
+﻿using Base;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Network;
+
+namespace Pacman
+{
+    class StateJoinLobby : IGameState
+    {
+        public StateJoinLobby()
+        {
+            
+        }
+
+        public void HandleInput(InputHelper inputHelper)
+        {
+
+        }
+
+        public IGameState TransitionTo()
+        {
+            return this;
+        }
+
+        public void Update(float dt)
+        {
+
+        }
+
+        public void Draw(DrawHelper drawHelper)
+        {
+
+        }
+
+    }
+}
