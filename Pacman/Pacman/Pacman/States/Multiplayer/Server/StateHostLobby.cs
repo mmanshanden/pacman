d918c5d6eab018772515f27b882e89072ca5cc34
Maurice Manshanden <m.manshanden@gmail.com>
2015-01-27 20:38:14 +0100

    Hide server when lobby full

--- Pacman/Pacman/Pacman/States/Multiplayer/Server/StateHostLobby.cs
+++ Pacman/Pacman/Pacman/States/Multiplayer/Server/StateHostLobby.cs
@@ -125,7 +125,7 @@ namespace Pacman
 
         public override void Update(float dt)
         {
-            if (this.players.Count < 3 && this.levelIndex == 1)
+            if (this.server.GetConnections().Count < 1 && this.levelIndex == 1)
                 this.server.Visible = true;
 
             else
